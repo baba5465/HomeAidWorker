e1f8163821b79d30153440a234013e8d072980de
Deepak Singh Baise <deepaksinghbaise@gmail.com>
2020-02-11 22:18:14 +0530

    added a string

--- app/src/main/java/com/baba/homeaidworker/MainActivity.java
+++ app/src/main/java/com/baba/homeaidworker/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    String flag="accept my pull request";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
